dfe531ae5fcea7a9a704eaca82e96649b3824dc3
Jeff Fisher <jeff.fisher@hostpapa.com>
2026-07-17 09:52:02 -0400

    Minor changes to make updating easier in the HostPapa environment. We force the external DB stuff to happen because our external DB is named db, so we change their name to dbBREAK. We do not force ubersmith_start.sh since we have our own customzied version. We make it so updatedb is skipped by default unless we pass 'updatedb' to the upgrade_ubersmith.sh script.

--- upgrade_ubersmith.sh
+++ upgrade_ubersmith.sh
@@ -4,12 +4,6 @@ set -e
 # SSL and Python development libraries are required.
 # See documentation at https://docs.ubersmith.com/article.php?id=231
 
-# Check if screen command exists
-if ! command -v screen &> /dev/null; then
-    echo "Error: GNU screen is not installed. Please install it and try again."
-    exit 1
-fi
-
 source ./find_python.sh
 source "$HOME"/.local/ubersmith_venv/bin/activate
 
@@ -17,8 +11,9 @@ echo "Installing Dependencies..."
 pip3 install --disable-pip-version-check -q -r requirements_pip.txt
 ansible-galaxy install -r requirements_ansible.yml
 
-echo "Starting screen and upgrading Ubersmith..."
+echo "Starting upgrading Ubersmith..."
 
-TIMESTAMP=$(date +%s)
-screen -L -Logfile "ubersmith_upgrade_${TIMESTAMP}.log" -dmS ubersmith_upgrade bash -c 'ansible-playbook -i ./hosts -e ansible_python_interpreter=$(which python3) -c local -t upgrade,upgrade_only upgrade_ubersmith.yml; exec bash'
-screen -r ubersmith_upgrade
+SKIPTAGS="updatedb"
+[[ "$1" == "updatedb" ]] && SKIPTAGS=""
+
+ansible-playbook -i ./hosts -e ansible_python_interpreter=$(which python3) -c local -t upgrade,upgrade_only --skip-tags "$SKIPTAGS" upgrade_ubersmith.yml
